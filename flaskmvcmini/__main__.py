67dea7f8af94b800199de20dd722deb09e0b3013
Rajendra Agrawal <19428701+Rajendra999@users.noreply.github.com>
2026-07-08 11:31:43 +0530

    Add files via upload

---
+++ flaskmvcmini/__main__.py
@@ -0,0 +1,9 @@
+from .cli import c_main
+
+def main():
+    print("flaskmvcmini v4.0")
+    print("Stage 01 OK")
+    c_main()
+
+if __name__ == "__main__":
+    main()
---
+++ flaskmvcmini/main.py
@@ -0,0 +1,11 @@
+"""
+Package Entry Point
+"""
+
+from .cli import c_main
+
+def main():
+c_main()
+
+if name == "main":
+main()
